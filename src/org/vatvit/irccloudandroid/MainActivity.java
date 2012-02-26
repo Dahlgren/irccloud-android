ce7a7fa112f024316e0e007d5bc6104af6917c86
Björn Dahlgren <bjorn@dahlgren.at>
2012-02-26 05:43:30 +0100

    Better handling of preferences, uses constants. Attempt to make better login but some due to some error in irc cloud lib the isLoggedIn might be reset to false when reopening app. Fixed duplicated servers by reseting server list on new login.

--- src/org/vatvit/irccloudandroid/MainActivity.java
+++ src/org/vatvit/irccloudandroid/MainActivity.java
@@ -14,24 +14,19 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	private static final String PREF_EMAIL = "email";
+	private static final String PREF_PASSWORD = "password";
 	private static final String TAG = "IRCCloudMainActivity";
 	private Client client;
-	/** Called when the activity is first created. */
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		MainActivity self = this;
+
 		final IRCCloudApplication app = ((IRCCloudApplication) getApplicationContext());
 		client = app.getClient();
-		
-		if(client.isLoggedIn()) {
-			Intent serversIntent = new Intent(this.getBaseContext(),
-					ServersActivity.class);
-			startActivity(serversIntent);
-			return;
-		}
-		
+
 		setContentView(R.layout.main);
 
 		final SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
@@ -39,46 +34,53 @@ public class MainActivity extends Activity {
 		final EditText emailField = (EditText) findViewById(R.id.emailField);
 		final EditText passwordField = (EditText) findViewById(R.id.passwordField);
 
-		String email = pref.getString("email", "");
-		String password = pref.getString("password", "");
-		
+		String email = pref.getString(PREF_EMAIL, "");
+		String password = pref.getString(PREF_PASSWORD, "");
+
 		loginButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(final View v) {
-				Toast.makeText(app.getApplicationContext(), "Trying to login",
-						Toast.LENGTH_SHORT).show();
-				(new Thread() {
-					@Override
-					public void run() {
-						Editor e = pref.edit();
-						String email = emailField.getText().toString().trim();
-						String password = passwordField.getText().toString().trim();
-						e.putString("email", email);
-						
-						if (client.login(email, password)) {
-							Log.d(TAG, "Login successful.");
-							e.putString("password", password);
-							Intent serversIntent = new Intent(v.getContext(),
-									ServersActivity.class);
-							startActivity(serversIntent);
-						} else {
-							Log.d(TAG, "Login failed.");
-							e.putString("password", "");
-							Toast.makeText(app.getApplicationContext(),
-									"Login failed", Toast.LENGTH_SHORT).show();
+				if (client.isLoggedIn()) {
+					showServers();
+				} else {
+					client.getServers().clear();
+					(new Thread() {
+						@Override
+						public void run() {
+							Editor e = pref.edit();
+							String email = emailField.getText().toString().trim();
+							String password = passwordField.getText().toString().trim();
+							e.putString(PREF_EMAIL, email);
+
+							if (client.login(email, password)) {
+								Log.d(TAG, "Login successful.");
+								e.putString(PREF_PASSWORD, password);
+								showServers();
+							} else {
+								Log.d(TAG, "Login failed.");
+								e.putString("password", "");
+								Toast.makeText(app.getApplicationContext(),
+										"Login failed", Toast.LENGTH_SHORT).show();
+							}
+							e.commit();
 						}
-						e.commit();
-					}
-				}).run();
+					}).run();
+				}
 
 			}
 		});
-		
+
+		emailField.setText(email);
+		passwordField.setText(password);
+
 		if (!(email.equals("") || password.equals(""))) {
 			loginButton.performClick();
-		} else {
-			emailField.setText(email);
-			passwordField.setText(password);
 		}
 	}
-	
+
+	private void showServers() {
+		Intent serversIntent = new Intent(this.getBaseContext(),
+				ServersActivity.class);
+		startActivity(serversIntent);
+	}
+
 }
