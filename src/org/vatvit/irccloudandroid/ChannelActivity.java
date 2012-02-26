ee222a22887b72de0f0d86a45be6a0e0f0463b24
Björn Dahlgren <bjorn@dahlgren.at>
2012-02-26 06:26:25 +0100

    Merged in json simple with a freshly built irccloud-java lib

--- src/org/vatvit/irccloudandroid/ChannelActivity.java
+++ src/org/vatvit/irccloudandroid/ChannelActivity.java
@@ -45,7 +45,7 @@ public class ChannelActivity extends Activity {
 		client = app.getClient();
 		Bundle extras = getIntent().getExtras();
 
-		int cid = extras.getInt("cid");
+		long cid = extras.getLong("cid");
 		for (Server serv : client.getServers()) {
 			if (serv.getCid() == cid) {
 				server = serv;
--- src/org/vatvit/irccloudandroid/ServerActivity.java
+++ src/org/vatvit/irccloudandroid/ServerActivity.java
@@ -37,7 +37,7 @@ public class ServerActivity extends ExpandableListActivity {
 		Client client = app.getClient();
 		Bundle extras = getIntent().getExtras();
 
-		int cid = extras.getInt("cid");
+		long cid = extras.getLong("cid");
 		for (Server serv : client.getServers()) {
 			if (serv.getCid() == cid) {
 				server = serv;
